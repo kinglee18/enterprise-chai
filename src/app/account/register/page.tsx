348cdb43cd97ef1557f48b6806a8e77b4246f228
King Lee <c-king.lee@hulu.com>
2024-07-19 17:27:37 -0600

    echai-195: terms and conditions

--- src/app/account/register/page.tsx
+++ src/app/account/register/page.tsx
@@ -3,80 +3,109 @@ import Image from 'next/image'
 import React from 'react'
 import {createUser} from "@/actions/account";
 import { useFormState, useFormStatus } from "react-dom";
-import {Button} from "@nextui-org/react";
-
+import {Button, ModalBody, ModalHeader} from "@nextui-org/react";
+import {Modal, ModalContent} from "@nextui-org/modal";
+import Terms from "@/app/account/register/terms";
+import "./style.css"
 const initialState = {
     message: "",
 };
 
-export default function Register() {
+export default function Register({
+    searchParams
+}) {
     const [state, formAction] = useFormState(createUser, initialState);
     const [checked, setChecked] = React.useState(true);
     return (
-        <form className='bg-white rounded-lg h-screen flex xl:pl-36 lg:pl-24' action={formAction}>
+        <>
+            <form className='bg-white rounded-lg h-screen flex xl:pl-36 lg:pl-24' action={formAction}>
 
-            <div className='w-7/12 flex-col p-10'>
-                <div className='w-9/12'>
-                    <a className="flex text-2xl place-items-center text-primarySmall font-medium" href={'/'}>
-                        <span>Enterprise</span>
-                        <Image src="/cup_logo.svg" alt="Logo" width={43} height={45} className={'mx-1 mb-3'}/>
-                        <span className={'font-medium text-primarySmall font-bold'}>CH</span>
-                        <span className={'font-medium text-greenLogo font-bold'}>AI</span>
-                    </a>
-                    <p className='welcome-back'>Create an account</p>
-                    <h3 className='mb-10'>Already have an account? <a
-                        href={'/login'}
-                        className={'text-primarySmall'}>Log-in</a></h3>
-                    <section className={'flex gap-3 flex-col'}>
-                        <div>
-                            <label className="text-gray-700">name</label>
+                <div className='w-7/12 flex-col p-10'>
+                    <div className='w-9/12'>
+                        <a className="flex text-2xl place-items-center text-primarySmall font-medium" href={'/'}>
+                            <span>Enterprise</span>
+                            <Image src="/cup_logo.svg" alt="Logo" width={43} height={45} className={'mx-1 mb-3'}/>
+                            <span className={'font-medium text-primarySmall font-bold'}>CH</span>
+                            <span className={'font-medium text-greenLogo font-bold'}>AI</span>
+                        </a>
+                        <p className='welcome-back'>Create an account</p>
+                        <h3 className='mb-10'>Already have an account? <a
+                            href={'/login'}
+                            className={'text-primarySmall'}>Log-in</a></h3>
+                        <section className={'flex gap-3 flex-col'}>
+                            <div>
+                                <label className="text-gray-700">name</label>
+                                <input
+                                    type="text"
+                                    className="form-input mt-1 block w-full"
+                                    placeholder="enter your name"
+                                    name={'username'}
+                                />
+                            </div>
+                            <div>
+                                <label className="text-gray-700">email</label>
+                                <input
+                                    type="text"
+                                    className="form-input mt-1 block w-full"
+                                    placeholder="enter your email"
+                                    name={'email'}
+                                />
+                            </div>
+                            <div>
+                                <label className="text-gray-700">password</label>
+                                <input
+                                    type="password"
+                                    className="form-input mt-1 block w-full"
+                                    placeholder="enter your password"
+                                    name={'password'}
+                                />
+                            </div>
+                        </section>
+                        <div className="flex gap-2 pt-4 pb-4 mb-8">
                             <input
-                                type="text"
-                                className="form-input mt-1 block w-full"
-                                placeholder="enter your name"
-                                name={'username'}
+                                type="checkbox"
+                                className='checkbox-login'
+                                checked={checked}
+                                onChange={(event) => {
+                                    setChecked(event.target.checked)
+                                }}
                             />
+                        I agree to EnterpriseCHAI’
+                            <a href={'/account/register?section=terms'} className={'text-primarySmall'}>Terms of Service</a>
                         </div>
-                        <div>
-                            <label className="text-gray-700">email</label>
-                            <input
-                                type="text"
-                                className="form-input mt-1 block w-full"
-                                placeholder="enter your email"
-                                name={'email'}
-                            />
-                        </div>
-                        <div>
-                            <label className="text-gray-700">password</label>
-                            <input
-                                type="password"
-                                className="form-input mt-1 block w-full"
-                                placeholder="enter your password"
-                                name={'password'}
-                            />
-                        </div>
-                    </section>
-                    <div className="flex gap-2 pt-4 pb-4 mb-8">
-                        <input
-                            type="checkbox"
-                            className='checkbox-login'
-                            checked={checked}
-                            onChange={(event) => {
-                                setChecked(event.target.checked)
-                            }}
-                        /> I agree to EnterpriseCHAI’s Terms & Conditions and Privacy Policy
+                        <p className={'text-warning'}>{state.message}</p>
+                        <SubmitButton checked={checked}/>
                     </div>
-                    <p className={'text-warning'}>{state.message}</p>
-                    <SubmitButton checked={checked}/>
                 </div>
-            </div>
 
-            <div className="w-5/12 h-full relative">
-                <div className="absolute inset-0">
-                    <Image src={'/Frame Image.png'} alt='bubbles photo' layout='fill' objectFit='cover' />
+                <div className="w-5/12 h-full relative">
+                    <div className="absolute inset-0">
+                        <Image src={'/Frame Image.png'} alt='bubbles photo' layout='fill' objectFit='cover' />
+                    </div>
                 </div>
-            </div>
-        </form>
+
+
+            </form>
+            { searchParams.section && <Modal
+                isOpen={true}
+                size={'full'}
+                scrollBehavior={"inside"}
+                closeButton={<a href={'/register'}>X</a>}
+            >
+                <ModalContent>
+                    {() => (<>
+                        <ModalHeader >
+                            <h1 className="text-3xl font-bold mb-6 text-gray-800">Beta Test Agreement</h1>
+
+                        </ModalHeader>
+                        <ModalBody>
+                            <Terms/>
+                        </ModalBody>
+                    </>
+                    )}
+                </ModalContent>
+            </Modal>}
+        </>
     )
 }
 
---
+++ src/app/account/register/terms.tsx
@@ -0,0 +1,115 @@
+export default function Terms() {
+    return <div className=" mx-auto p-6 bg-white shadow-lg rounded-lg">
+        <div >
+            <div className="mb-6 text-gray-700">
+                <p className="mb-4">
+                    This Beta Test Agreement
+                    (&rdquo;<span className={'font-semibold'}>Agreement</span>&rdquo;) is between EnterpriseChai, Inc., a Delaware corporation (&rdquo;<span className={'font-semibold'}>Licensor</span>&rdquo;), and the entity identified on the Registration Page (&rdquo;<span className={'font-semibold'}>Beta Participant&rdquo;</span>).
+                </p>
+                <p className="mb-4 ">
+                    PLEASE READ THIS AGREEMENT CAREFULLY BEFORE USING THE SERVICES. BY CLICKING THE &rdquo;I ACCEPT&rdquo; BUTTON ON THE REGISTRATION PAGE, BETA PARTICIPANT ACKNOWLEDGES ITS ACCEPTANCE OF THIS AGREEMENT, REPRESENTS THAT THE PERSON CLICKING THE &rdquo;I ACCEPT&rdquo; BUTTON ON THE REGISTRATION PAGE IS AUTHORIZED TO ENTER INTO THIS AGREEMENT ON BETA PARTICIPANT&lsquo;S BEHALF, AND AGREES THIS AGREEMENT IS ENFORCEABLE AGAINST BETA PARTICIPANT TO THE SAME EXTENT AS ANY WRITTEN NEGOTIATED AGREEMENT SIGNED BY BETA PARTICIPANT. IF BETA PARTICIPANT DOES NOT AGREE TO THE TERMS OF THIS AGREEMENT, BETA PARTICIPANT MAY NOT USE THE SERVICES.
+                </p>
+            </div>
+
+            <div className="mb-6">
+                <ol>
+                    <li className=" ">
+                        <span className=" font-semibold mb-4">Definitions</span>
+                        <p className="mb-2 text-gray-700">As used in this Agreement, the following terms have the following meanings:</p>
+                        <ol className="list-disc pl-6 text-gray-700">
+                            <li >&rdquo;Beta Test&rdquo; means the testing and evaluation of the Services by Beta Participant during the Term.</li>
+                            <li >&rdquo;Content&rdquo; means the information, documents, software, products, and services made available to Beta Participant in the course of using the Services.</li>
+                            <li >&rdquo;Customer Data&rdquo; means any non-public data about any of Beta Participant’s customers or clients that is provided, input, or submitted by Beta Participant or Users to the Services in the course of using the Services.</li>
+                            <li >&rdquo;Licensor Technology&rdquo; means all of Licensor’s proprietary technology (including software, processes, algorithms, user interfaces, know-how, techniques, designs, and other intangible technical material or information) made available to Beta Participant by Licensor in providing the Services.</li>
+                            <li >&rdquo;Effective Date&rdquo; means the date upon which Beta Participant clicks an “I Accept&rdquo; button on the Registration Page.</li>
+                            <li >&rdquo;Intellectual Property Rights&rdquo; means any unpatented inventions, patent applications, patents, design rights, copyrights, trademarks, service marks, trade names, domain name rights, mask work rights, know-how and other trade secret rights, and all other intellectual property rights, derivatives thereof, and forms of protection of a similar nature anywhere in the world.</li>
+                            <li >&rdquo;Registration Page&rdquo; means the page on https://www.enterprisechai.com/ through which Licensor enables Beta Participant to register to participate in the Beta Test.</li>
+                            <li >&rdquo;Services&rdquo; means Licensor’s hosted customer success tools and agent solution and services, accessible via https://www.enterprisechai.com/ (or any other means made available to Beta Participant by Licensor), to which Beta Participant is being granted access for the Beta Test under this Agreement, including the Licensor Technology and Content.</li>
+                            <li >&rdquo;User(s)&rdquo; means Beta Participant’s employees, representatives, consultants, contractors, or agents who are authorized by Beta Participant to use the Services and have been supplied user identifications and passwords either by Licensor or, with Licensor’s prior approval, by Beta Participant.</li>
+                        </ol>
+                    </li>
+                    <li>
+                        <span>Use of the Services</span>
+                        <ol className="list-disc pl-6 text-gray-700">
+                            <li >Subject to the terms and conditions of this Agreement, Licensor hereby grants Beta Participant a non-exclusive, non-transferable, non-sublicensable, revocable, limited right to access and use the Services, solely: (a) for Beta Participant’s internal business purposes; and (b) by five Users at any given time. All rights not expressly granted to Beta Participant herein are reserved by Licensor.</li>
+                            <li >Beta Participant may use the Services only as part of, and in connection with, the Beta Test and will not: (a) license, sublicense, sell, resell, transfer, assign, distribute, or otherwise commercially exploit or make available to any third party the Services in any way; (b) modify, alter, tamper with, or make derivative works based upon the Services; (c) reverse engineer, disassemble, decompile, translate, or reproduce the Services; (d) access the Services in order to (i) build a competitive product or service, (ii) build a product using similar ideas, features, functions, or graphics of the Services, or (iii) copy any ideas, features, functions, or graphics of the Services; (e) interfere with or disrupt the integrity or performance of the Services or the data contained therein; or (f) attempt to gain unauthorized access to the Services or their related systems or networks, including by attempting to circumvent any user limits or other timing, use, or functionality restrictions built into the Services.</li>
+                            <li >If Licensor notifies Beta Participant that a fee that is payable for use of the Services hereunder, Beta Participant may not use the Services until such fee has been paid by Beta Participant to Licensor. Beta Participant will pay such fee, within 30 days after receipt of Licensor’s invoice, by ACH/EFT or bank wire to an account designed by Licensor.</li>
+                        </ol>
+                    </li>
+                    <li>
+                        <span>Beta Participant Responsibilities</span>
+                        <ol className="list-disc pl-6 ">
+                            <li  >
+                                For so long as Beta Participant continues to use the Services, Beta Participant will (and will ensure Users) actively engage with and provide Licensor feedback about the Services, including without limitation by: (a) providing Licensor information about any errors or defects in the Services found by Beta Participant; (b) informing Licensor of any suggested changes, modifications, or improvements to the Services; and (c) providing Licensor with all information reasonably requested by Licensor for purposes of evaluating the results of the Beta Test. As used in this Agreement, “Feedback” means, collectively, all of the foregoing information and any other any comments, criticisms, reports, or feedback, whether in oral or written form, Beta Participant (including Users) provides to Licensor regarding the function, features, and other characteristics of the Services, including, without limitation, any errors, problems, or defects in, or suggestions for changes or improvements to, the Services. Licensor will be free to exercise all rights in any Feedback, without restriction and without compensating Beta Participant.
+                            </li>
+                            <li  >Beta Participant is responsible for all activity occurring under Beta Participant’s User accounts and will abide by all applicable local, state, national, and foreign, laws, treaties, and regulations in connection with Beta Participant’s use of the Services, including those related to data privacy, data security, and the transmission of technical or personal data.</li>
+                            <li  >Beta Participant is responsible for properly using the Services and taking steps to maintain appropriate security, protection, and backup Customer Data. Beta Participant will: (a) safeguard all passwords relating to the Services, keep all such passwords strictly confidential and provide such passwords only to authorized employees or personnel who need to know such information for the purpose of using the Services; (b) notify Licensor immediately of any unauthorized use of any password or account or any other known or suspected breach of security; and (c) report to Licensor immediately, and use reasonable efforts to stop, any unauthorized copying or distribution of Content that is known or suspected by Beta Participant or Users.
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <span>Customer Data</span>
+                        <p>Customer hereby grants Licensor the right to access and use Customer Data solely: (a) for the provision of the Services; (b) to prevent or address service or technical problems; (c) in connection with support matters; and (d) to review and improve the performance of the Services. Upon termination, Beta Participant’s right to access or use Customer Data through the Services immediately ceases, and Licensor will have no obligation to maintain or forward any Customer Data and may delete Customer Data immediately upon termination. As between Licensor and Beta Participant, Beta Participant owns all right, title, and interest (including all Intellectual Property Rights) in and to the Customer Data and all rights not expressly granted to Licensor with respect to Customer Data herein are reserved by Beta Participant.</p>
+                    </li>
+                    <li>
+                        <span>Licensor Ownership</span>
+                        <p>As between Licensor and Beta Participant, Licensor owns all right, title, and interest (including all Intellectual Property Rights) in and to the Services (including any changes, modifications, or corrections thereto). Beta Participant understands and agrees that Beta Participant will gain no right, title, or interest (including any Intellectual Property Rights) in or to the Services by virtue of Beta Participant’s Feedback and that Beta Participant does not now have, will not have, and will never claim to have, any proprietary rights (including any Intellectual Property Rights) in or to the Services (or any changes, modifications, or corrections thereto). If Beta Participant is ever held or deemed to hold any right in or to the Services (or any changes, modifications, or corrections thereto) by virtue of Beta Participant’s Feedback, then Beta Participant hereby irrevocably assigns to Licensor all such rights. If any such right cannot be so assigned, Beta Participant hereby grants to Licensor an exclusive, irrevocable, perpetual, worldwide, royalty-free license, with right to sublicense through multiple tiers of sublicensees, to use, reproduce, distribute, create derivative works of, publicly perform and display, in any medium or format, whether now known or later developed, any and all property that is subject to such rights for further development and commercialization of the Services.
+                        </p>
+                    </li>
+                    <li>
+                        <span>Term; Termination
+                        </span>
+                        <p>The term of this Agreement commences on the Effective Date and continues for a period of one hundred eighty (180) days thereafter (the “Term”), unless earlier terminated as provided herein. This Agreement may be terminated at any time either by Licensor or by Beta Participant by written notice to the other. Licensor may terminate Beta Participant’s or any User’s password, account, or use of the Services at any time. Beta Participant agrees and acknowledges that Licensor has no obligation to retain Customer Data, and may delete Customer Data immediately upon termination.
+                        </p>
+                    </li>
+                    <li>
+                        <span>Confidentiality</span>
+                        <p>The Services and the Content are confidential and proprietary to Licensor and its licensors. Beta Participant agrees not to disclose any part of the Services or any non-public information relating to the Services (including without limitation the results of use or the Services or any Feedback) to any third party without Licensor’s prior written permission. Beta Participant acknowledges and agrees that unauthorized use or disclosure of such information would cause irreparable harm and significant injury to Licensor that would be difficult to ascertain. Accordingly, Beta Participant agrees that Licensor will have the right to obtain injunctive or other equitable relief to enforce obligations under this Agreement without limiting any other rights or remedies.
+                        </p>
+                    </li>
+                    <li>
+                        <span>Indemnification
+                        </span>
+                        <p>Beta Participant will defend, indemnify, and hold harmless Licensor, its affiliates, and their respective employees, officers, directors, and representatives from and against any third-party claims, and any losses, liabilities, costs, and expenses (including reasonable attorneys’ fees) incurred by Licensor in connection therewith, relating to Beta Participant’s (including any User’s) use of the Services.
+                        </p>
+                    </li>
+                    <li>
+                        <span>Disclaimer of Warranties</span>
+                        <ol className="list-disc pl-6">
+                            <li >
+                                LICENSOR MAKES NO REPRESENTATION, WARRANTY, OR GUARANTY AS TO THE RELIABILITY, TIMELINESS, QUALITY, SUITABILITY, AVAILABILITY, ACCURACY, OR COMPLETENESS OF THE SERVICES OR ANY CONTENT. THE SERVICES AND ALL CONTENT ARE PROVIDED TO BETA PARTICIPANT ON AN “AS IS” BASIS. ALL CONDITIONS, REPRESENTATIONS AND WARRANTIES, WHETHER EXPRESS, IMPLIED, STATUTORY OR OTHERWISE, INCLUDING, WITHOUT LIMITATION, ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT OF THIRD PARTY RIGHTS, ARE HEREBY DISCLAIMED TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW BY LICENSOR.
+                            </li>
+                            <li>BETA PARTICIPANT ACKNOWLEDGES AND AGREES THAT: (A) THE SERVICES ARE NOT IN FINAL FORM, HAVE NOT BEEN RELEASED FOR BY LICENSOR FOR COMMERCIAL USE, AND MAY CONTAIN DEFECTS, ERRORS, DESIGN FLAWS, OR OTHER PROBLEMS WHICH CANNOT OR WILL NOT BE CORRECTED; (B) THE USE OF THE SERVICES MAY NOT BE SECURE, TIMELY, UNINTERRUPTED, OR ERROR-FREE; (C) THE SERVICES MAY NOT OPERATE IN COMBINATION WITH ANY OTHER HARDWARE, SOFTWARE, SYSTEM, OR DATA; (D) ANY STORED DATA MAY NOT BE ACCURATE OR RELIABLE; (E) THE SERVICES MAY NOT MEET BETA PARTICIPANT’S REQUIREMENTS OR EXPECTATIONS; (F) THE SERVICES OR THE SERVER(S) THAT MAKE THE SERVICES AVAILABLE MAY NOT BE FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS; AND (G) THE SERVICES MAY BE SUBJECT TO LIMITATIONS, DELAYS, AND OTHER PROBLEMS INHERENT IN THE USE OF THE INTERNET AND ELECTRONIC COMMUNICATIONS.
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <span>Limitation of Liability
+                        </span>
+                        <p>IN NO EVENT WILL LICENSOR BE LIABLE FOR ANY DAMAGES WHATSOEVER, INCLUDING BUT NOT LIMITED TO ANY DIRECT, INDIRECT, PUNITIVE, SPECIAL, EXEMPLARY, INCIDENTAL, CONSEQUENTIAL, OR OTHER DAMAGES OF ANY TYPE OR KIND, ARISING OUT OF OR IN ANY WAY CONNECTED WITH THIS AGREEMENT, EVEN IF LICENSOR HAS BEEN ADVISED OF THE POSSIBILITY OF, OR SHOULD REASONABLY FORESEE, SUCH DAMAGES.
+                        </p>
+                    </li>
+                    <li>
+                        <span>Modification to Terms
+                        </span>
+                        <p>Licensor reserves the right to modify the terms of this Agreement or its policies relating to the Services at any time, effective upon posting of an updated version of this Agreement or such polices on the Services. Beta Participant is responsible for regularly reviewing this Agreement and such policies. Continued use of the Services after any such changes will constitute Beta Participant’s consent to such changes.
+                        </p>
+                    </li>
+                    <li>
+                        <span>General</span>
+                        <ol className="list-disc pl-6 ">
+                            <li >
+                                This Agreement will be governed by the laws of the State of California, without reference to conflict of laws principles. The parties hereby irrevocably: (a) submit to the jurisdiction of the federal courts sitting in the Northern District of California or any California state courts in the counties of San Francisco or Santa Clara, for the purpose of any suit, action, or proceeding arising out of this Agreement; and (b) waive the defense of an inconvenient forum to the maintenance of any such suit, action, or proceeding.
+                            </li>
+                            <li >Licensor may provide Beta Participant with notice, including notice regarding changes to the terms of this Agreement, or the termination hereof, by email, regular mail, or postings on the Services. Notices provided by Licensor by posting on the Services will be effective upon posting, and notices sent by email or regular mail will be effective when sent by Licensor. Beta Participant may give notice to Licensor solely by email to <a className={'text-blue-400'} href={'mailto: pallavi@enterprisechai.com'}>pallavi@enterprisechai.com</a> (or to such other email address, or by such other means, as Licensor may designate from time to time). Notices provided by Beta Participant will be deemed effective when received by Licensor.
+                            </li>
+                            <li >If any provision of this Agreement is held by a court of competent jurisdiction to be invalid or unenforceable, then such provision(s) will be construed, as nearly as possible, to reflect the intentions of the invalid or unenforceable provision(s), with all other provisions remaining in full force and effect. No joint venture, partnership, employment, or agency relationship exists between Beta Participant and Licensor as a result of this Agreement or the use of the Services. The failure of Licensor to enforce any right or provision in this Agreement will not constitute a waiver of such right or provision unless acknowledged and agreed to by Licensor in writing. This Agreement comprises the entire agreement between Beta Participant and Licensor and supersedes all prior or contemporaneous negotiations, discussions, or agreements, whether written or oral, between the parties regarding the subject matter contained herein. This Agreement may not be assigned by Beta Participant without the prior written approval of Licensor.
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+    </div>
+}
