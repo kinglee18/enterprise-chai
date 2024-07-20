5569100d44f16d2e63922a3485345cc90d31828e
King Lee <c-king.lee@hulu.com>
2024-07-19 23:27:12 -0600

    feedback button removed

--- src/components/FeedbackButtons.tsx
+++ src/components/FeedbackButtons.tsx
@@ -2,9 +2,8 @@
 export default function FeedbackButtons() {
     return (
         <div className="flex items-center   justify-around pt-12">
-            <div className=" flex justify-between w-[640px] px-12">
+            <div className=" flex justify-center ">
                 <a className="btn-session" href={'/home/csm'}>Go Back</a>
-                <a className="btn-feedback">Share feedback</a>
             </div>
         </div>
     )
