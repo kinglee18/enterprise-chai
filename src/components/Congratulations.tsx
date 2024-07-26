d71f53ecc649a606d3db0df561d44a135c0d8df9
King Lee <c-king.lee@hulu.com>
2024-07-25 19:54:29 -0600

    summary sizes

--- src/components/Congratulations.tsx
+++ src/components/Congratulations.tsx
@@ -9,7 +9,7 @@ export default function Congratulations({summaryInfo}) {
             <div className=" items-center font-bold gap-1 pt-9">
                 <span className="text-2xl text-primarySmall">{summaryInfo.session.journey_phase}</span>
             </div>
-            <p className="text-[#52525B] font-montserrat text-sm font-normal leading-[25px] tracking-[0.14px]">
+            <p className="text-[#52525B] font-montserrat text-sm font-normal leading-[25px] tracking-[0.14px] my-2">
                 {`${createdAT} | #${summaryInfo.session.session_number}`}
             </p>
         </div>
