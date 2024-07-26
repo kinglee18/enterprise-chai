97e6591535963435ef8f6b7e991eb08c381a0056
King Lee <c-king.lee@hulu.com>
2024-07-25 20:15:45 -0600

    summary sizes

--- src/app/session/[id]/finished/SessionWarning.tsx
+++ src/app/session/[id]/finished/SessionWarning.tsx
@@ -16,22 +16,26 @@ export default function SessionWarning({summaryInfo, transcript, id }: any ) {
                   <div className={'text-sm'} dangerouslySetInnerHTML={{__html: summaryInfo?.summary_details?.conversation_summary.replace(/\n/g, '<br>')}} />
               </div>
               <div className={'flex gap-6  w-3/6 justify-end px-10'}>
-                  {summaryInfo?.summary_details?.session_score && (<div className={'flex flex-col items-center'}>
-                      <div class="w-32 h-32 rounded-full bg-lightBlue flex items-center justify-center">
-                          <span class=" text-base	 font-bold">{summaryInfo.summary_details.session_score}</span>
-                      </div>
-                      <div>
-                          <h3 className="text-md  mb-2 font-sm w-full">Session Score</h3>
-                      </div>
-                  </div>)}
-                  {summaryInfo?.summary_details?.overall_sentiment && (<div className={'flex flex-col items-center'}>
-                      <div class="w-32 h-32 rounded-full bg-lightGreen flex items-center justify-center">
-                          <span class=" text-base	 font-bold">{summaryInfo?.summary_details?.overall_sentiment}</span>
-                      </div>
-                      <div>
-                          <h3 className="text-md  mb-2 font-sm w-full"> Overall Sentiment</h3>
-                      </div>
-                  </div>)}
+                  <div className={'absolute flex gap-5'}>
+
+                      {summaryInfo?.summary_details?.session_score && (<div className={'  '}>
+                          <div class="w-28 h-28 rounded-full bg-lightBlue flex items-center justify-center">
+                              <span class=" text-base	 font-bold">{summaryInfo.summary_details.session_score}</span>
+                          </div>
+                          <div>
+                              <h3 className="text-md  mb-2 font-sm w-full">Session Score</h3>
+                          </div>
+                      </div>)}
+                      {summaryInfo?.summary_details?.overall_sentiment && (<div className={'flex flex-col items-center '}>
+                          <div class="w-28 h-28 rounded-full bg-lightGreen flex items-center justify-center">
+                              <span class=" text-base capitalize	 font-bold">{summaryInfo?.summary_details?.overall_sentiment}</span>
+                          </div>
+                          <div>
+                              <h3 className="text-md  mb-2 font-sm w-full"> Overall Sentiment</h3>
+                          </div>
+                      </div>)}
+                  </div>
+
               </div>
           </div>
         }
@@ -129,7 +133,7 @@ export default function SessionWarning({summaryInfo, transcript, id }: any ) {
 const MessageComponent = ({ data , summaryInfo }) => {
 
     return (
-        <div className="m-4 min-h-96 h-[550px] overflow-y-auto	">
+        <div className="m-4 min-h-96 h-[550px] overflow-y-auto	pr-2">
             <div className={
                 ' '
             }>
